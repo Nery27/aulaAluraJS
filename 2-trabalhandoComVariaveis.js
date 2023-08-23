a1224455e1eb13b3357777cca99283cd8d1d29a5
Rodrigo <rodrigonery27@hotmail.com>
2023-08-23 14:24:36 -0300

    primeiro commit

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 44;
+const nome = "Rodrigo";
+
+//não fazer
+//mes = "Janeiro";
